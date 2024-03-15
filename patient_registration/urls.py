608a4b58c545131f3109f621cade4ab7bb226e35
saleeja <saleejas2@gmail.com>
2024-03-15 17:18:23 +0530

    second

--- patient_registration/urls.py
+++ patient_registration/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_patient, name='add_patient'),
-    path('list/', views.patient_list, name='patient_list'),
+    path('add-patients/', views.add_patient, name='add_patient'),
+    path('list-patients/', views.patient_list, name='patient_list'),
     path('add-medical-record/', views.add_medical_record, name='add_medical_record'),
     path('medical-records/', views.medical_record_list, name='medical_record_list'),
     path('upload-record/', views.upload_record, name='upload_record'),
